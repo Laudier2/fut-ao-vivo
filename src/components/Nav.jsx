05c77447123d8db1af6c9b7f2e4c8f77f7086b11
Laudier2 <trabalhandooline2019@hotmail.com>
2023-07-28 00:07:32 -0300

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,8 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+//import axios from 'axios'
+import React from 'react'
 
 export default function Nav() {
 
-  const [update, setUpdate] = useState()
 
   /*useEffect(() => {
      (async () => {
@@ -19,7 +18,7 @@ export default function Nav() {
       <nav className="navbar navbar-dark bg-primary" style={{ height: 80 }}>
         <div class="container-fluid">
           <a className="navbar-brand btn btn-dark" href="/">Home</a>
-          <button className="navbar-brand btn btn-dark" onClick={update} >Atualize os Jogos</button>
+          <button className="navbar-brand btn btn-dark" >Atualize os Jogos</button>
         </div>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShn-fSb2i9aExJ2nGz9gJtPuzYWCaZo24Pg8S4ccB3UtZGAi_ZDBIcjjhVBHU7XMHPzAY&usqp=CAU" className="rounded mx-auto d-block" alt="img" />
 
